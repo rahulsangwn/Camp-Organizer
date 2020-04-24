882f8a1c156ced7913fc31054820494543d3f415
Rahul Sangwan <rahul.sangwan3@gmail.com>
2020-04-24 19:36:09 +0530

    prepared for interfacing

--- Project.API/App_Start/WebApiConfig.cs
+++ Project.API/App_Start/WebApiConfig.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Project.API
 {
@@ -19,6 +21,18 @@ namespace Project.API
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // Enable CORS for the Angular App
+            var cors = new EnableCorsAttribute("http://localhost:4200", "*", "*");
+            // using the * as a wildcard for headers and methods
+            // allowing all headers and methods from this origin
+            config.EnableCors(cors);
+
+            // Set JSON formatter as default one and remove XmlFormatter
+            var jsonFormatter = config.Formatters.JsonFormatter;
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            jsonFormatter.SerializerSettings.DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc;
         }
     }
 }
